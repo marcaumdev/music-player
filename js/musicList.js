d2dcf92fced900bd1c893a86986fd4d682d00a57
Marcos Vinícius <marcaumdev@gmail.com>
2024-04-29 21:28:50 -0300

    player iniciado

---
+++ js/musicList.js
@@ -0,0 +1,20 @@
+let allMusic = [
+    {
+      name: "Primeiro lugar do pódio",
+      artist: "Kyan",
+      img: "primeiro-lugar-do-podio",
+      src: "Kyan - Primeiro Lugar do Pódio _ GOALS"
+    },
+    {
+      name: "CALA BOCA, TRABALHO",
+      artist: "Rudies Flacko feat. Aka rasta",
+      img: "cala-boca-trabalho",
+      src: "RUDIES FLACKO - CALA-BOCA, TRABALHO ft. AKA RASTA"
+    },
+    {
+      name: "A noite chega",
+      artist: "G.A feat Ryu the runner e Mc Koringa",
+      img: "a-noite-chega",
+      src: "3. G.A - chega a noite ft. Ryu The Runner e Mc Koringa (Sua Vez De Escutar)"
+    },
+  ];
